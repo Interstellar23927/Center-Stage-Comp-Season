837c975b1a37b3ea27227f805b8009fe5c86acfe
Interstellar23927 <152816988+Interstellar23927@users.noreply.github.com>
2024-01-07 14:56:41 -0500

    Update RedBackdrop.java

--- Autonomous/RedBackdrop.java
+++ Autonomous/RedBackdrop.java
@@ -1,8 +1,3 @@
-package org.firstinspires.ftc.teamcode.Autonomous;
-
-public class RedBackdrop {
-  package org.firstinspires.ftc.teamcode.Autonomous;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
