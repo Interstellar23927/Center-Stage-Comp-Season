5ec73c3f665b8542fcddb55dcdd8267b9dac014b
Interstellar23927 <152816988+Interstellar23927@users.noreply.github.com>
2024-01-17 19:19:57 -0500

    Update RedBackdrop.java

--- Autonomous/RedBackdrop.java
+++ Autonomous/RedBackdrop.java
@@ -1,5 +1,65 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
-public class RedBackdrop {
-}
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+@Autonomous
+public class RedBackdrop extends LinearOpMode {
+    public DcMotor frontRight;
+    public DcMotor frontLeft;
+    public DcMotor backRight;
+    public DcMotor backLeft;
+
+
+
+    @Override
+    public void runOpMode () throws InterruptedException {
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        frontRight.setPower(-.4);
+        frontLeft.setPower(-.4);
+        backRight.setPower(-.4);
+        backLeft.setPower(-.4);
+
+        sleep(600);
+
+        frontRight.setPower(0);
+        frontLeft.setPower(0);
+        backRight.setPower(0);
+        backLeft.setPower(0);
+
+        frontRight.setPower(.5);
+        frontLeft.setPower(-.5);
+        backRight.setPower(.5);
+        backLeft.setPower(-.5);
+
+        sleep(600);
+
+        frontRight.setPower(0);
+        frontLeft.setPower(0);
+        backRight.setPower(0);
+        backLeft.setPower(0);
+
+        frontRight.setPower(-.5);
+        frontLeft.setPower(-.5);
+        backRight.setPower(-.5);
+        backLeft.setPower(-.5);
+
+        sleep(1000);
+
+        frontRight.setPower(0);
+        frontLeft.setPower(0);
+        backRight.setPower(0);
+        backLeft.setPower(0);
+    }
+}
