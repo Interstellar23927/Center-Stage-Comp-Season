bd7e53b100d03914ad741e269d3340793b4c3586
Interstellar23927 <152816988+Interstellar23927@users.noreply.github.com>
2024-01-23 19:56:48 -0500

    Update Slides.java

--- TeleOp/Slides.java
+++ TeleOp/Slides.java
@@ -1,37 +1,36 @@
-package org.firstinspires.ftc.teamcode.TeleOp;
+/*package org.firstinspires.ftc.teamcode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import org.firstinspires.ftc.teamcode.HardwareRobot;
 
 
 public class Slides {
     HardwareRobot robot;
-
-
     public Slides(HardwareRobot r) {
         robot = r;
     }
 
-    public void Update(Gamepad gamepad1, Gamepad gamepad2) {
+
+ public void Update(Gamepad gamepad1, Gamepad gamepad2) {
 
         //change buttons
 
         if (gamepad2.dpad_up) {
-            robot.slideRight.setPower(-0.5);
-            robot.slideLeft.setPower(-0.5);
+            robot.slideRight.setPower(0.5);
+            robot.slideLeft.setPower(0.5);
         }
-        else if (gamepad2.dpad_up) {
-            robot.slideRight.setPower(0.0);
-            robot.slideLeft.setPower(0.0);
-        }
+        robot.slideRight.setPower(0.0);
+        robot.slideLeft.setPower(0.0);
+
         if (gamepad2.dpad_down) {
-            robot.slideRight.setPower(0.2);
-            robot.slideLeft.setPower(0.2);
+            robot.slideRight.setPower(-0.2);
+            robot.slideLeft.setPower(-0.2);
         }
-        else if(gamepad2.dpad_down) {
-            robot.slideRight.setPower(0.0);
-            robot.slideLeft.setPower(0.0);
+
+        robot.slideRight.setPower(0.0);
+        robot.slideLeft.setPower(0.0);
         }
-    }
-}
+    }*/
+
+
 
 
