9fd8645747449a5bb10edcf95de629b36eb731ad
Interstellar23927 <152816988+Interstellar23927@users.noreply.github.com>
2024-01-25 21:33:53 -0500

    Update Suspension.java

--- TeleOp/Suspension.java
+++ TeleOp/Suspension.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
+
 import com.qualcomm.robotcore.hardware.Gamepad;
 import org.firstinspires.ftc.teamcode.HardwareRobot;
 
@@ -9,11 +10,26 @@ public class Suspension {
         robot = r;
     }
     public void Update(Gamepad gamepad1, Gamepad gamepad2) {
-        if (gamepad2.right_bumper) { //change buttons
-            robot.leftHook.setPower(-0.5);
-            robot.rightHook.setPower(+0.5);
+        if (gamepad2.left_trigger == 0) {
+            robot.leftHook.setPower(0.05);
+            robot.rightHook.setPower(-0.05);
+        } else {
+            robot.leftHook.setPower(-gamepad2.right_trigger);
+            robot.rightHook.setPower(gamepad2.right_trigger);
+
+            robot.leftHook.setPower(gamepad2.left_trigger);
+            robot.rightHook.setPower(-gamepad2.left_trigger);
         }
-        robot.leftHook.setPower(0.0);
-        robot.rightHook.setPower(0.0);
     }
+
+//        if (gamepad2.right_bumper) { //change buttons
+//            robot.leftHook.setPower(-1);
+//            robot.rightHook.setPower(1);
+//        } else if (gamepad2.left_bumper) {
+//            robot.leftHook.setPower(1);
+//            robot.rightHook.setPower(-1);
+//        } else
+//        robot.leftHook.setPower(0.0);
+//        robot.rightHook.setPower(0.0);
+//    }
 }
